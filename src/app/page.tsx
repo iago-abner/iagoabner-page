b6ba5839ff8b7b5fa7d6e31b0968969566fbcfb1
iago_abner <iagoabner.dev@gmail.com>
2022-12-30 16:59:51 -0300

    other: remove contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import About from "../components/About/About";
+// import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home";
 import Navbar from "../components/Navbar/Navbar";
+import Projects from "../components/Projects/Projects";
+import Skills from "../components/Skills/Skills";
 import "../styles/page.scss";
 
 export default function Page() {
@@ -9,6 +13,10 @@ export default function Page() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
+      <Projects />
+      {/* <Contact /> */}
+      <Footer />
     </>
   );
 }
