17235c5e30a966e9ad21e14c48e0dece3cbd5355
iago_abner <iagoabner.dev@gmail.com>
2022-12-30 16:49:07 -0300

    feat: added interface for icons and projects data

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,17 @@
-export type User = {
-  id: number
-  name: string
+import { StaticImageData } from "next/image";
+import { IconBaseProps } from "react-icons";
+
+export interface IWork {
+  id: number,
+  img: StaticImageData,
+  name: string,
+  category: string,
+  gh: string,
+  host: string,
+}
+
+export interface IIcon {
+  id: number;
+  link: string;
+  icon: IconBaseProps;
 }
