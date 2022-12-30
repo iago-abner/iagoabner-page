6adf280479591e8c58e99ba5e19b910c060fcaa4
iago_abner <iagoabner.dev@gmail.com>
2022-12-30 16:57:45 -0300

    refactore: data format

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -21,23 +21,40 @@ import {
   SiTypescript,
 } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
-import work1 from "../assets/images/work1.png";
-import work2 from "../assets/images/work2.png";
-import work3 from "../assets/images/work3.png";
-import work4 from "../assets/images/work4.png";
-import work5 from "../assets/images/work5.png";
+import dtmoney from "../assets/images/dt-money.png";
+import ignews from "../assets/images/ignews.png";
+import imgExpressRentalx from "../assets/images/img-express.png";
+import imgNestMicroservices from "../assets/images/img-nestjs.png";
+import sensor from "../assets/images/nextjs-sensor-monitoring.png";
 import work6 from "../assets/images/work6.png";
+import { IIcon } from "../interfaces";
 
 export const navLinks = ["home", "about", "skills", "portfolio", "contact"];
 
-export const socialIcons = [<FaGithub />, <FaLinkedin />, <SiGmail />];
+export const socialIcons: IIcon[] = [
+  {
+    id: 1,
+    icon: <FaGithub />,
+    link: "https://github.com/iago-abner",
+  },
+  {
+    id: 2,
+    icon: <FaLinkedin />,
+    link: "https://www.linkedin.com/in/iago-abner/",
+  },
+  {
+    id: 3,
+    icon: <SiGmail />,
+    link: "mailto:iagoabner.dev@gmail.com",
+  },
+];
 
 export const bios = [
   {
     id: 1,
     icon: <FaUser />,
     key: "Name",
-    value: "Iago",
+    value: "Iago Abner",
   },
   {
     id: 2,
@@ -80,43 +97,55 @@ export const experiences = [
 export const workImages = [
   {
     id: 1,
-    img: work1,
+    img: sensor,
     name: "project 1",
     category: "web",
+    gh: "https://github.com/iago-abner/next-monitoring",
+    host: "https://next-temp-iago-abner.vercel.app/",
   },
   {
     id: 2,
-    img: work2,
+    img: dtmoney,
     name: "project 2",
     category: "web",
+    gh: "https://github.com/iago-abner/dt-money",
+    host: "https://github.com/iago-abner",
   },
   {
     id: 3,
-    img: work3,
+    img: ignews,
     name: "project 3",
     category: "web",
+    gh: "https://github.com/iago-abner/ignews",
+    host: "http://ignews-iago-abner.vercel.app/",
   },
   {
     id: 4,
-    img: work4,
+    img: imgExpressRentalx,
     name: "project 4",
-    category: "app",
+    category: "api",
+    gh: "https://github.com/iago-abner/rentalx",
+    host: "https://github.com/iago-abner",
   },
   {
     id: 5,
-    img: work5,
+    img: imgNestMicroservices,
     name: "project 5",
-    category: "app",
+    category: "api",
+    gh: "https://github.com/iago-abner/microservices-nest",
+    host: "https://github.com/iago-abner",
   },
   {
     id: 6,
     img: work6,
     name: "project 6",
-    category: "design",
+    category: "mobile",
+    gh: "https://github.com/iago-abner",
+    host: "https://github.com/iago-abner",
   },
 ];
 
-export const workNavs = ["All", "Web", "App", "Design"];
+export const workNavs = ["All", "Web", "Api", "Mobile"];
 
 export const contacts = [
   {
